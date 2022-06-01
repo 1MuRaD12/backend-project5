42857b2e174f8c06daf067b86d73cf7e80431778
1MuRaD12 <tu71tfsws@code.edu.az>
2022-06-01 11:00:26 +0300

    first commit

---
+++ AP204_Pronia/Services/LayoutService.cs
@@ -0,0 +1,24 @@
+﻿using AP204_Pronia.DAL;
+using AP204_Pronia.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AP204_Pronia.Services
+{
+    public class LayoutService
+    {
+        private readonly AppDbContext context;
+
+        public LayoutService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Setting> getData()
+        {
+            Setting setting = await context.settings.FirstOrDefaultAsync();
+
+            return setting;
+        }
+    }
+}
